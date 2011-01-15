1448b657dca24845cb035eba178831383232602f
lolautruche <lolautruche@61bc4249-67c4-4ab4-b911-91924637b6c6>
2011-01-15 17:52:31 +0000

    Translation files (unstranslated and fre-FR)
    git-svn-id: http://svn.projects.ez.no/jvamazonadvertising/trunk@15 61bc4249-67c4-4ab4-b911-91924637b6c6

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>design/standard/class/datatype/jvamazonid</name>
+    <message>
+        <source>Reference field for ASIN search</source>
+        <translation>Champ de référence pour la recherche d&apos;ASIN</translation>
+    </message>
+    <message>
+        <source>Enter attribute identifiers as for object name (ie. &amp;lt;my_attribute_identifier&amp;gt;)</source>
+        <translation>Entrez les identifiants d&apos;attribut comme pour le nom d&apos;objet (ex. &amp;lt;mon_attribute_identifier&amp;gt;)</translation>
+    </message>
+    <message>
+        <source>If left empty, will be content object name</source>
+        <translation>Si laissé vide, sera le nom de l&apos;objet de contenu</translation>
+    </message>
+    <message>
+        <source>Automatically search ASIN on publish if object attribute is left empty (default value)</source>
+        <translation>Chercher automatiquement l&apos;ASIN à la publication if l&apos;attribut d&apos;objet est laissé vide (valeur par défaut)</translation>
+    </message>
+    <message>
+        <source>Amazon SearchIndex (category) to search into</source>
+        <translation>Amazon SearchIndex (catégorie) dans lequel chercher</translation>
+    </message>
+    <message>
+        <source>Enter a valid search index (product category).</source>
+        <translation>Entrez un index de recherche valide (catégorie de produit).</translation>
+    </message>
+    <message>
+        <source>Search indexes are listed here</source>
+        <translation>Les index de recherche sont listés ici</translation>
+    </message>
+    <message>
+        <source>Amazon BrowseNode (sub-category) to search into</source>
+        <translation>Amazon BrowseNode (sous-catégorie) dans lequel chercher</translation>
+    </message>
+    <message>
+        <source>Enter a valid browse node (product location in Amazon catalog). Leave empty if not applicable</source>
+        <translation>Entrez un BrowseNode valide (emplacement du produit dans le catalogue Amazon). Laissez vide is non applicable</translation>
+    </message>
+    <message>
+        <source>Find a browse nodes non-exhaustive list here</source>
+        <translation>Vous trouverez une liste non exhaustive de browse nodes ici</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>Non</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Oui</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/content/datatype/jvamazonid</name>
+    <message>
+        <source>Search Amazon ID</source>
+        <translation>Rechercher un ID Amazon</translation>
+    </message>
+    <message>
+        <source>Automatically search Amazon ID on publish if empty</source>
+        <translation>Chercher automatiquement un ID Amazon à la publication si le champ est vide</translation>
+    </message>
+    <message>
+        <source>Product name</source>
+        <translation>Nom du produit</translation>
+    </message>
+    <message>
+        <source>Product image</source>
+        <translation>Image du produit</translation>
+    </message>
+    <message>
+        <source>Main category</source>
+        <translation>Catégorie principale</translation>
+    </message>
+    <message>
+        <source>Product group</source>
+        <translation>Groupe de produit</translation>
+    </message>
+    <message>
+        <source>Product link</source>
+        <translation>Lien du produit</translation>
+    </message>
+    <message>
+        <source>No product has been found in Amazon catalog for the search query &quot;%searchquery&quot;</source>
+        <translation>Aucun produit n&apos;a été trouvé dans le catalogue Amazon pour la requête &quot;%searchquery&quot;</translation>
+    </message>
+    <message>
+        <source>Product search results</source>
+        <translation>Résultats de recherche de produit</translation>
+    </message>
+    <message>
+        <source>Click on the right product name to fill the field</source>
+        <translation>Cliquez sur le bon nom de produit pour remplir le champ</translation>
+    </message>
+</context>
+<context>
+    <name>extension/jvamazonadvertising/error</name>
+    <message>
+        <location filename="jvamazonidtype.php" line="211"/>
+        <source>Invalid Amazon product ID</source>
+        <translation>ID de produit Amazon invalide</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/untranslated/translation.ts
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>design/standard/class/datatype/jvamazonid</name>
+    <message>
+        <source>Reference field for ASIN search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter attribute identifiers as for object name (ie. &amp;lt;my_attribute_identifier&amp;gt;)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If left empty, will be content object name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Automatically search ASIN on publish if object attribute is left empty (default value)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Amazon SearchIndex (category) to search into</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter a valid search index (product category).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search indexes are listed here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Amazon BrowseNode (sub-category) to search into</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter a valid browse node (product location in Amazon catalog). Leave empty if not applicable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Find a browse nodes non-exhaustive list here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/content/datatype/jvamazonid</name>
+    <message>
+        <source>Search Amazon ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Automatically search Amazon ID on publish if empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Product name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Product image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Main category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Product group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Product link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No product has been found in Amazon catalog for the search query &quot;%searchquery&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Product search results</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Click on the right product name to fill the field</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>extension/jvamazonadvertising/error</name>
+    <message>
+        <location filename="jvamazonidtype.php" line="211"/>
+        <source>Invalid Amazon product ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
