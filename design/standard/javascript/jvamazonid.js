9b254049fde11aaa5e097be388be124b50ca5447
lolautruche <lolautruche@61bc4249-67c4-4ab4-b911-91924637b6c6>
2011-01-15 17:22:21 +0000

    Removed hard coded attribute ID in JS file
    git-svn-id: http://svn.projects.ez.no/jvamazonadvertising/trunk@11 61bc4249-67c4-4ab4-b911-91924637b6c6

--- design/standard/javascript/jvamazonid.js
+++ design/standard/javascript/jvamazonid.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	$('a.jvamazonid_result').click(function() {
-		var aID = $(this).attr('id').split('_');
-		var attributeID = aID[1];
-		var asin = aID[2];
-		var nameAttribute = 'ContentObjectAttribute_jvamazonid_data_text_111192';
-		
-		$('input[name="'+nameAttribute+'"]').val(asin);
-	});
+    $('a.jvamazonid_result').click(function() {
+        var aID = $(this).attr('id').split('_');
+        var attributeID = aID[1];
+        var asin = aID[2];
+        var nameAttribute = 'ContentObjectAttribute_jvamazonid_data_text_'+attributeID;
+        
+        $('input[name="'+nameAttribute+'"]').val(asin);
+    });
 });
